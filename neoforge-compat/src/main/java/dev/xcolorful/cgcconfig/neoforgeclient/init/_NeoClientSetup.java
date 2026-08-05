550cd0a50f0f3a04deea7e14294838c121a3e65f
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-06 00:49:58 +0800

    Port to 1.21.1neoforge

--- neoforge-compat/src/main/java/dev/xcolorful/cgcconfig/neoforgeclient/init/_NeoClientSetup.java
+++ neoforge-compat/src/main/java/dev/xcolorful/cgcconfig/neoforgeclient/init/_NeoClientSetup.java
@@ -4,10 +4,10 @@ import dev.xcolorful.cgcconfig.CgcConfig;
 import dev.xcolorful.cgcconfig.client.init._ClientSetup;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
-@Mod.EventBusSubscriber(value = Dist.CLIENT, modid = CgcConfig.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(value = Dist.CLIENT, modid = CgcConfig.MOD_ID)
 public class _NeoClientSetup {
 
     private static final  _ClientSetup CLIENT_SETUP = _ClientSetup.get();
