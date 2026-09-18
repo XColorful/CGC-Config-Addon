4d1a23ec51b1bf332b28d65e543a102dab11fa93
XiaoColorful <XColor_ful@outlook.com>
2026-09-19 00:27:17 +0800

    Remove GPL-3.0 headers from data format definitions

--- core/src/main/java/dev/xcolorful/cgcconfig/core/api/minecraft/input/_CustomInputKeyTag.java
+++ core/src/main/java/dev/xcolorful/cgcconfig/core/api/minecraft/input/_CustomInputKeyTag.java
@@ -1,17 +1,3 @@
-/*
- * Copyright (c) 2024-2026 MCModderAnchor (https://github.com/MCModderAnchor)
- * SPDX-License-Identifier: GPL-3.0-only
- *
- * Source: https://github.com/MCModderAnchor/TACZ
- */
-
-/*
- * Copyright (c) 2026 XiaoColorful (https://github.com/XColorful)
- * SPDX-License-Identifier: GPL-3.0-only
- *
- * Source: https://github.com/XColorful/Custom-Gun-Continued
- */
-
 package dev.xcolorful.cgcconfig.core.api.minecraft.input;
 
 import dev.xcolorful.cgcconfig.CgcConfig;
